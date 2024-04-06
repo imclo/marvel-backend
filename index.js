72646565c47429ef750e6c81259bda09713e5935
Chloé Tan <chloe@MacBook-Pro-de-Chloe.local>
2024-04-07 00:51:23 +0200

    seventh commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
